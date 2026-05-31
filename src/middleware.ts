4e92a44cb677f4c4b99fe952ccc935d5dfa7653d
Jeremy Lebon <jeremy@motorscompagny.com>
2026-06-01 01:30:46 +0200

    fix: middleware vide

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,5 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
 
-export async function middleware(request: NextRequest) {
-  const token = request.cookies.get('sb-ykvumfzmrsngdevucrty-auth-token')
-
-  if (request.nextUrl.pathname.startsWith('/dashboard') && !token) {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
-
+export async function middleware() {
   return NextResponse.next()
-}
-
-export const config = {
-  matcher: ['/dashboard/:path*'],
 }
